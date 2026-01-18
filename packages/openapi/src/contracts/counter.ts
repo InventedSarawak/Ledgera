69066533c40a82e6592c54fd5ff382f968a6740a
Vedant Kesarwani <relativityeismc2@gmail.com>
2026-01-18 12:12:36 +0530

    Fix format in frontend and added some commands

--- packages/openapi/src/contracts/counter.ts
+++ packages/openapi/src/contracts/counter.ts
@@ -1,52 +1,52 @@
-import { initContract } from "@ts-rest/core";
-import { ZCounter } from "@ledgera/zod";
-import { getSecurityMetadata } from "../utils.js";
+import { initContract } from '@ts-rest/core'
+import { ZCounter } from '@ledgera/zod'
+import { getSecurityMetadata } from '../utils.js'
 
-const c = initContract();
-const metadata = getSecurityMetadata();
+const c = initContract()
+const metadata = getSecurityMetadata()
 
 export const counterContract = c.router({
-  getCounter: {
-    summary: "Get counter",
-    path: "/counter",
-    method: "GET",
-    description: "Get or create counter for authenticated user",
-    responses: {
-      200: ZCounter,
+    getCounter: {
+        summary: 'Get counter',
+        path: '/counter',
+        method: 'GET',
+        description: 'Get or create counter for authenticated user',
+        responses: {
+            200: ZCounter
+        },
+        metadata: metadata
     },
-    metadata: metadata,
-  },
-  incrementCounter: {
-    summary: "Increment counter",
-    path: "/counter/increment",
-    method: "POST",
-    body: null,
-    description: "Increment counter value by 1",
-    responses: {
-      200: ZCounter,
+    incrementCounter: {
+        summary: 'Increment counter',
+        path: '/counter/increment',
+        method: 'POST',
+        body: null,
+        description: 'Increment counter value by 1',
+        responses: {
+            200: ZCounter
+        },
+        metadata: metadata
     },
-    metadata: metadata,
-  },
-  decrementCounter: {
-    summary: "Decrement counter",
-    path: "/counter/decrement",
-    method: "POST",
-    body: null,
-    description: "Decrement counter value by 1",
-    responses: {
-      200: ZCounter,
+    decrementCounter: {
+        summary: 'Decrement counter',
+        path: '/counter/decrement',
+        method: 'POST',
+        body: null,
+        description: 'Decrement counter value by 1',
+        responses: {
+            200: ZCounter
+        },
+        metadata: metadata
     },
-    metadata: metadata,
-  },
-  resetCounter: {
-    summary: "Reset counter",
-    path: "/counter/reset",
-    method: "POST",
-    body: null,
-    description: "Reset counter value to 0",
-    responses: {
-      200: ZCounter,
-    },
-    metadata: metadata,
-  },
-});
+    resetCounter: {
+        summary: 'Reset counter',
+        path: '/counter/reset',
+        method: 'POST',
+        body: null,
+        description: 'Reset counter value to 0',
+        responses: {
+            200: ZCounter
+        },
+        metadata: metadata
+    }
+})
